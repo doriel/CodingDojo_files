a90944ae978bea223e24f9dcc2228a97786b9f14
Doriel Fonseca <doriel3572@gmail.com>
2016-09-24 09:48:00 +0100

    Adding the project files

---
+++ project/js/script.js
@@ -0,0 +1,86 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'template/template.html',
+		link: function(scope, elem, attrs) {
+			scope.start = function() {
+				scope.id = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+			scope.checkAnswer = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+				} else {
+					scope.correctAns = false;
+				}
+
+				scope.answerMode = false;
+			};
+
+			scope.nextQuestion = function() {
+				scope.id++;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question: "Qual é a capital de Angola?",
+			options: ["Luanda", "Cabinda", "Lunda-Norte", "Zaire"],
+			answer: 0
+		},
+		{
+			question: "Quantas províncias existem em Angola?",
+			options: ["20", "10", "15", "18"],
+			answer: 3
+		},
+		{
+			question: "Quando foi a sua inependência?",
+			options: ["11/9/2001", "2/2/1994", "11/11/1975", "18/1/1972"],
+			answer: 2
+		}
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
